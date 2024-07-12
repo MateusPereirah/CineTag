375cdbb5c6e1b86009f4eb8cd729cfcc6a1440ee
MateusPereirah <mateus.pereira1@outlook.com.br>
2024-07-12 06:32:16 -0300

    Rotas e uso do JSON

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,12 +1,26 @@
 
+import Banner from "components/Banner";
 import Cabecalho from "../../components/Cabecalho";
 import Rodape from "../../components/Rodape";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
+import styles from './Inicio.module.css'
+import videos from 'json/db.json';
 
 function Inicio(){
     return(
         <>
-            <Cabecalho />
-            <Rodape/>
+            
+            <Banner imagem="home"/>
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id}/>
+                })}
+            </section>
+            
         </> 
     );
 }
